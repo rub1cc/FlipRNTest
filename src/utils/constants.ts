32490ee734271548bc54227e1107481cb5d35393
farazaulia <raz.muhammad14@gmail.com>
2022-08-08 11:38:32 +0700

    fix(helpers): fix formatDateToHumandReadable doesn't show correct date in Android

---
+++ src/utils/constants.ts
@@ -0,0 +1,38 @@
+export const SORT_OPTIONS = {
+  '': {
+    value: '',
+    label: 'URUTKAN'
+  },
+
+  'beneficiary_name:asc': {
+    value: 'beneficiary_name:asc',
+    label: 'Nama A-Z'
+  },
+  'beneficiary_name:desc': {
+    value: 'beneficiary_name:desc',
+    label: 'Nama Z-A'
+  },
+  'created_at:asc': {
+    value: 'created_at:asc',
+    label: 'Tanggal Terbaru'
+  },
+  'created_at:desc': {
+    value: 'created_at:desc',
+    label: 'Tanggal Terlama'
+  }
+}
+
+export const MONTH_NAMES = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember'
+]
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,20 +1,29 @@
+import { Platform } from 'react-native'
+import { MONTH_NAMES } from './constants'
+
 export const formatNumberToRupiah = (value: number): string => {
-  const rupiah = new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
-    minimumFractionDigits: 0
-  }).format(value)
+  const rupiah =
+    'Rp' + value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')
   return rupiah
 }
 
-export const formatDateStringToHumanReadable = (date: string): string => {
-  const dateObj = new Date(date.split(' ')[0])
-  const formattedDate = dateObj.toLocaleDateString('id-ID', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  })
-  return formattedDate
+export const formatDateStringToHumanReadable = (stringDate: string): string => {
+  const dateObj = new Date(stringDate.split(' ')[0])
+
+  if (Platform.OS === 'ios') {
+    return dateObj.toLocaleDateString('id-ID', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  const date = dateObj.getDate()
+  const month = MONTH_NAMES[dateObj.getMonth()]
+  const year = dateObj.getFullYear()
+
+  return `${date} ${month} ${year}`
+
 }
 
 export const capitalize = (str: string): string => {
@@ -22,4 +31,34 @@ export const capitalize = (str: string): string => {
     return str.toUpperCase()
   }
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+const sortName = (a: string, b: string, order: string): number => {
+  if (order === 'asc') {
+    return a.localeCompare(b)
+  }
+  return b.localeCompare(a)
+}
+
+const sortDate = (a: string, b: string, order: string): number => {
+  const aValue = new Date(a).getTime()
+  const bValue = new Date(b).getTime()
+  if (order === 'asc') {
+    return aValue - bValue
+  }
+  return bValue - aValue
+}
+
+export const sortArrayBy = (arr: any[], criteria: string): any[] => {
+  const sorted = arr.sort((a, b) => {
+    const [key, order] = criteria.split(':')
+
+    if (key === 'beneficiary_name') {
+      return sortName(a[key], b[key], order)
+    } else if (key === 'created_at') {
+      return sortDate(a[key], b[key], order)
+    }
+    return 0
+  })
+  return sorted
 }
